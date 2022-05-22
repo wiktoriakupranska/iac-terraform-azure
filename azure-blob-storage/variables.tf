038ad0a4de3dbd1254c5ff26a4bd80b4f0bf6d5a
wiktoriakupranska <91828963+wiktoriakupranska@users.noreply.github.com>
2022-05-22 13:45:51 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,37 @@
-
+variable "azurem_resource_group-name" {
+	type = string
+	default = "Wiktoria75911"
+}
+variable "azurem_resource_group_location" {
+	type = string
+	default = "North Europe"
+}
+variable "account_kind" {
+  type = string
+  default = "Storage"
+}
+variable "account_tier" {
+	type = string
+  default = "Standard"
+}
+variable "account_replication_type" {
+	type = string
+	default = "LRS"
+}
+variable "azurerm_storage_container_kontener" {
+    type = string
+    default = "nazwaKontenera"
+}
+variable "container_access_type" {
+    type = string
+    default = "blob"
+}
+variable "azurerm_storage_blob_skladzik" {
+    type = string
+    default = "nazwaSkladzika"
+}
+variable "azurerm_storage_blob_type" {
+    type = string
+    default = "Block"
+  
+}
