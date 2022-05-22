549a00a054554106b86a62d78f30bab512b83fc4
wiktoriakupranska <91828963+wiktoriakupranska@users.noreply.github.com>
2022-05-22 13:13:49 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,1 +1,35 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
+  tags = {
+    environment = "staging"
+  }
+}
+resource "azurerm_app_service_plan" "example" {
+  name                = "api-appserviceplan-pro"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-linux-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name = azurerm_storage_account.example.name
+  service_plan_id      = azurerm_service_plan.example.id
+
+  site_config {}
+}
