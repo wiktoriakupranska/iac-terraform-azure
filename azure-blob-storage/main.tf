0e43e5e500c21f08d7bc4de36459b8ac5f1fb6ff
wiktoriakupranska <91828963+wiktoriakupranska@users.noreply.github.com>
2022-05-22 13:35:39 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,24 @@
-
+resource "azurerm_resource_group" "example" {
+  name     = var.azurem_resource_group-name
+  location = var.azurem_resource_group_location
+}
+resource "azurerm_storage_account" "example" {
+  name                     = var.azurem_resource_group-name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             =  var.account_tier
+  account_replication_type = var.account_replication_type
+  
+}
+resource "azurerm_storage_container" "example" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
+}
